832987e9cb467e62b5077dc5d86d57897eed6ac9
Thomas Isaksson <tom@localhost.localdomain>
2017-01-13 23:16:33 +0100

    commenting

--- src/iosched.c
+++ src/iosched.c
@@ -9,15 +9,10 @@
 #include <stdbool.h>
 #include <signal.h>
 
-#define time_mean(a) start=clock(); a; diff = clock() - start; msec = (float)diff * 1000 / CLOCKS_PER_SEC; times[i] = msec; 
-#define time(a) start=clock(); a; diff = clock() - start; msec = (float)diff * 1000 / CLOCKS_PER_SEC; printf("%f\n", msec); 
-#define time_mean_create(a) start=clock(); a; diff = clock() - start; msec = (float)diff * 1000 / CLOCKS_PER_SEC; create_mean[i] = msec; 
-#define time_mean_remove(a) start=clock(); a; diff = clock() - start; msec = (float)diff * 1000 / CLOCKS_PER_SEC; remove_mean[i] = msec; 
-
 #define FILES_PER_THREAD 10
 #define NUM_TESTS 40
 
-#define realtime(a) gettimeofday(&s,NULL); a; gettimeofday(&e,NULL); sec=(e.tv_sec-s.tv_sec); microsec=((sec*1000000) + e.tv_usec)-(s.tv_usec); printf("%ld\n", microsec);
+#define time(a) gettimeofday(&s,NULL); a; gettimeofday(&e,NULL); sec=(e.tv_sec-s.tv_sec); microsec=((sec*1000000) + e.tv_usec)-(s.tv_usec); printf("%ld\n", microsec);
 
 
 void *create_file(void *filename);
@@ -47,38 +42,6 @@ int main(int argc, char *argv[]) {
 
 	int num_threads = strtol(argv[1], NULL, 10);
 
-/*
-	gettimeofday(&s, NULL);
-	sleep(1);
-	gettimeofday(&e, NULL);
-
-
-	secs_used = (e.tv_sec - s.tv_sec);
-	micros_used = ((secs_used*1000000) + e.tv_usec) - (s.tv_usec);
-*/
-/*
-	// GET CREATE/REMOVE AVERAGE OVER 10 TEST RUNS
-	float create_mean[NUM_TESTS];
-	float remove_mean[NUM_TESTS];
-	for (int i = 0; i<NUM_TESTS; i++) {
-		time_mean_create(threadfile_creator(num_threads));
-		time_mean_remove(threadfile_deleter(num_threads));
-	}
-
-	float mean_create = 0;
-	for (int i = 0; i<NUM_TESTS; i++) {
-		mean_create += create_mean[i];
-	}
-	printf("mean create time %f\n", mean_create/NUM_TESTS);
-
-	float mean_remove = 0;
-	for (int i = 0; i<NUM_TESTS; i++) {
-		mean_remove += remove_mean[i];
-	}
-
-	printf("mean remove time %f\n", mean_remove/NUM_TESTS);
-*/
-
 
 	pthread_t threadmaster;
 	pthread_create(&threadmaster, NULL, &thread_master, &num_threads);
@@ -87,7 +50,6 @@ int main(int argc, char *argv[]) {
 	fprintf(stderr, "Master starting massive superwrite\n");
 	// begin massive annoying write and time it
 	time(system("dd if=/dev/zero of=test bs=64k count=32k conv=fdatasync"));
-	//time(system("dd if=/dev/sda of=/dev/null"));
 
 	if (running == 1) {
 		__sync_fetch_and_sub(&running, 1);
@@ -98,15 +60,6 @@ int main(int argc, char *argv[]) {
 	pthread_join(threadmaster, NULL);
 
 
-
-
-
-
-// SINGLE RUN
-/*
-	time(threadfile_creator(num_threads));
-	time(threadfile_deleter(num_threads));
-*/
 	return 0;
 
 }
@@ -122,12 +75,16 @@ void sig_handler(int signo) {
 
 }
 
+/* Thread that manages smaller threads.
+ * Ensures that there is a constant stream of smaller threads
+ * working as long as the main thread is busy.
+ * usage: num_threads - the amount of threads to be spawned each round
+ */
 void *thread_master(void *num_threads) {
 	while(running) {
 		if (thread_count == 0) {
-			//sleep(1);
-			realtime(threadfile_creator(*(int*)num_threads));
-			realtime(threadfile_deleter(*(int*)num_threads));
+			time(threadfile_creator(*(int*)num_threads));
+			time(threadfile_deleter(*(int*)num_threads));
 
 		} 
 	}
@@ -136,9 +93,12 @@ void *thread_master(void *num_threads) {
 
 }
 
+/* Spawns a number of threads and dispatches them
+ * to create files. Waits for them all to finish before returning.
+ * usage: num_threads - the number of threads
+ */
 void *threadfile_creator(int num_threads) {
-	// create files
-	//thread_count += num_threads;
+	// create threads
 	for (int i = 0; i<num_threads; i++) {
 		char *filename = malloc(sizeof(char)*10);
 		sprintf(filename, "efbefc%d", i);
@@ -153,8 +113,6 @@ void *threadfile_creator(int num_threads) {
 		}
 	}
 
-	//create_file("efbefc0");
-
 	do {
     	__sync_synchronize();
   	} while (thread_count);
@@ -163,9 +121,12 @@ void *threadfile_creator(int num_threads) {
 	
 }
 
+/* Spawns a number of threads and dispatches them
+ * to remove files. Waits for them all to finish before returning.
+ * usage: num_threads - the number of threads
+ */
 void *threadfile_deleter(int num_threads) {
 	// remove files
-	//thread_count += num_threads;
 	for (int i = 0; i<num_threads; i++) {
 		char *filename = malloc(sizeof(char)*10);
 		sprintf(filename, "efbefc%d", i);
@@ -189,7 +150,10 @@ void *threadfile_deleter(int num_threads) {
 
 
 
-// may only be called from thread
+/* Single function to create files.
+ * May only be called from within a thread
+ * usage: filename - the basename of the file to be created
+ */
 void *create_file(void *filename) {
 	for (int i = 0; i<FILES_PER_THREAD; i++) {
 		char *fn = malloc(sizeof(char)*strlen(filename)+10);
@@ -201,19 +165,16 @@ void *create_file(void *filename) {
 			close(fd);
 		}
 		free(fn);
-		//printf("creating file %s\n", fn);
 	}
-  	//char l[100];
-  	//sprintf(l, "dd if=/dev/zero of=test%s bs=64k count=2k conv=fdatasync", (char*)filename);
-  	//system(l);
-
   	free(filename);
   	__sync_fetch_and_sub(&thread_count,1);
 	return NULL;
 }
 
-// may only be called from thread
-void *remove_file(void *filename) {
+/* Single function to remove files.
+ * May only be called from within a thread
+ * usage: filename - the basename of the file to be removed
+ */void *remove_file(void *filename) {
 	for (int i = 0; i<FILES_PER_THREAD; i++) {
 		char *fn = malloc(sizeof(char)*strlen(filename)+10);
 		sprintf(fn, "%s%d", (char*)filename, i);
@@ -221,12 +182,7 @@ void *remove_file(void *filename) {
 			perror("remove file");
 		}
 		free(fn);
-		//printf("removing file %s\n", fn);
 	}
-  	//char l[100];
-  	//sprintf(l, "dd if=/dev/zero of=test%s bs=64k count=2k conv=fdatasync", (char*)filename);
-  	//system(l);
-
 
   	free(filename);
   	__sync_fetch_and_sub(&thread_count,1);
